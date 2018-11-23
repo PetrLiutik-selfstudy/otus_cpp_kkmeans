452b602af793ecbca9880e1cf4197b97a0369319
PetrLiutik <petr_lutik@mail.ru>
2018-11-23 23:39:42 +0300

    Добавил gnuplot

--- src/plot.cpp
+++ src/plot.cpp
@@ -1,14 +1,14 @@
 #include "plot.h"
 
-//#include <gnuplot-iostream.h>
+#include "gnuplot-iostream.h"
 
 void plot(const plot_data_t& plot_data) {
-//  Gnuplot gp("gnuplot -persist");
-//  gp << "set terminal png size 800,600\n";
-//  gp << "set output 'output.png'\n";
-//  gp << "set key outside\n";
-//  gp << "plot";
-//  for(const auto& it: plot_data)
-//    gp << gp.file1d(it.second) << "with points pointtype 7 ps 2 title 'cluster" << +it.first << "',";
-//  gp << std::endl;
+  Gnuplot gp("gnuplot -persist");
+  gp << "set terminal png size 800,600\n";
+  gp << "set output 'output.png'\n";
+  gp << "set key outside\n";
+  gp << "plot";
+  for(const auto& it: plot_data)
+    gp << gp.file1d(it.second) << "with points pointtype 7 ps 2 title 'cluster" << +it.first << "',";
+  gp << std::endl;
 }
